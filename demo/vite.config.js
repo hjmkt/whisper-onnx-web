ead017b071e3167d4240a97c62dfba0a9835f69d
Hajime Kato <hjmkt3126@gmail.com>
2023-04-22 18:43:55 +0900

    Update docs

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -2,25 +2,51 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-        plugins: [react()],
-        assetsInclude: [
-            "**/*.onnx",
-            "**/vocab.json",
-            //"**/*.wasm",
-            "**/*.gz",
-            "**/*.compressed",
-        ],
-        resolve: {
-            alias: {
-                "@core": path.resolve(__dirname, "../src"),
-                "@models": path.resolve(__dirname, "../models"),
-            },
-            modules: [
-                path.resolve(__dirname, "../src/node_modules"),
-                "node_nodules",
+export default defineConfig(({ command, mode }) => {
+    if (mode == "production") {
+        return {
+            base: "/whisper-onnx-web",
+            plugins: [react()],
+            assetsInclude: [
+                "**/*.onnx",
+                "**/vocab.json",
+                //"**/*.wasm",
+                "**/*.gz",
+                "**/*.compressed",
             ],
-        },
-        build: { sourcemap: true },
+            resolve: {
+                alias: {
+                    "@core": path.resolve(__dirname, "../src"),
+                    "@models": path.resolve(__dirname, "../models"),
+                },
+                modules: [
+                    path.resolve(__dirname, "../src/node_modules"),
+                    "node_nodules",
+                ],
+            },
+            build: { sourcemap: true },
+        };
+    } else {
+        return {
+            plugins: [react()],
+            assetsInclude: [
+                "**/*.onnx",
+                "**/vocab.json",
+                //"**/*.wasm",
+                "**/*.gz",
+                "**/*.compressed",
+            ],
+            resolve: {
+                alias: {
+                    "@core": path.resolve(__dirname, "../src"),
+                    "@models": path.resolve(__dirname, "../models"),
+                },
+                modules: [
+                    path.resolve(__dirname, "../src/node_modules"),
+                    "node_nodules",
+                ],
+            },
+            build: { sourcemap: true },
+        };
     }
-);
+});
